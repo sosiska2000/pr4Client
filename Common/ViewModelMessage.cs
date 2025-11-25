85ad732c2268d7b9a0cdd6152b584f58707dc808
sosiska2000 <maratoleg2006@gmail.com>
2025-11-25 18:02:07 +0500

    ViewModelMessage

---
+++ Common/ViewModelMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Common
+{
+    [Serializable]
+    public class ViewModelMessage
+    {
+        public string Command { get; set; }
+        public string Data { get; set; }
+
+        public ViewModelMessage(string command, string data)
+        {
+            this.Command = command;
+            this.Data = data;
+        }
+    }
+}
---
+++ Common/ViewModelSend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Common
+{
+    [Serializable]
+    public class ViewModelSend
+    {
+        public string Message { get; set; }
+        public int Id { get; set; }
+
+        public ViewModelSend(string message, int id)
+        {
+            this.Message = message;
+            this.Id = id;
+        }
+    }
+}

58afc501197d55270e87308a82fa4527d4181ef9
sosiska2000 <maratoleg2006@gmail.com>
2025-11-25 18:02:39 +0500

    FileInfoFTP

---
+++ Common/FileInfoFTP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Common
+{
+    [Serializable]
+    public class FileInfoFTP
+    {
+        public byte[] Data { get; set; }
+        public string Name { get; set; }
+
+        public FileInfoFTP(byte[] data, string name)
+        {
+            this.Data = data;
+            this.Name = name;
+        }
+    }
+}
